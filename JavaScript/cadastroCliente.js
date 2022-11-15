3912ad91c6f9d99f02de81f8a86b2e3d6dd61772
Tiago zay <tiagozay@gmail.com>
2022-11-15 14:11:24 -0300

    Separando cadastro
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ JavaScript/cadastroCliente.js
@@ -0,0 +1,116 @@
+const formularioCadastro = document.querySelector(".formularioDeCadastro");
+
+formularioDeCadastro.onsubmit = (event) => {
+
+    event.preventDefault();
+
+    const validacao = validaUsuario(
+        formularioCadastro.nome,
+        formularioCadastro.email, 
+        formularioCadastro.senha,
+        formularioCadastro.confSenha
+    );
+
+    if(!validacao){
+        return;
+    }
+
+    const httpService = new HttpService();
+
+    let loader = document.querySelector("#loaderCadastrar");
+
+    loader.classList.remove("display-none");
+
+    httpService.postFormulario(formularioCadastro, 'back-end/cadastraUsuario.php')
+    .then( () => {
+
+        formularioCadastro.reset();
+
+        loader.classList.add("display-none");
+        
+        location.href = "index.php";
+    } )
+    .catch( resposta => {
+
+        loader.classList.add("display-none");
+
+        if(resposta.message == 'usuario_ja_cadastrado'){
+            abrirMensagemDeErroDoInput(formularioCadastro.email, "E-mail inválido ou já cadastrado.");
+        }else{
+            fecharMensagemDeErroDoInput(formularioCadastro.email);
+        }
+    } )
+
+}
+
+function validaUsuario(campoNome, campoEmail, campoSenha, campoConfSenha)
+{
+    let nome = campoNome.value;
+    let email = campoEmail.value;
+    let senha = campoSenha.value;
+    let confSenha = campoConfSenha.value;
+
+    if(nome.trim().length == 0){
+        abrirMensagemDeErroDoInput(campoNome, "Informe seu nome.");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoNome);
+
+    if( nome.length < 3 || nome.length > 80){
+        abrirMensagemDeErroDoInput(campoNome, "Tamanho do nome inválido!");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoNome);
+
+    if(email.trim().length == 0){
+        abrirMensagemDeErroDoInput(campoEmail, "Informe um e-mail.");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoEmail);
+
+    if(email.trim().length > 260){
+        abrirMensagemDeErroDoInput(campoEmail, "E-mail inválido");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoEmail);
+
+    if(senha.trim().length == 0){
+        abrirMensagemDeErroDoInput(campoSenha, "Informe a senha.");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoSenha);
+
+    if(senha.trim().length < 8){
+        abrirMensagemDeErroDoInput(campoSenha, "A senha deve conter no mínimo 8 caracteres.");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoSenha);
+
+    if(confSenha.trim().length == 0){
+        abrirMensagemDeErroDoInput(campoConfSenha, "Confirme a senha.");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoConfSenha);
+
+    if(confSenha != senha){
+        abrirMensagemDeErroDoInput(campoConfSenha, "As senhas não coincidem.");
+        return false;
+    }
+    fecharMensagemDeErroDoInput(campoConfSenha);
+
+    return true;
+}
+
+function abrirMensagemDeErroDoInput(input, mensagem){
+    input.focus();
+
+    let msgErro = document.querySelector(`#msgErro-${input.name}`);
+    msgErro.classList.remove('display-none');
+    msgErro.innerHTML = mensagem;
+}
+
+function fecharMensagemDeErroDoInput(input){
+    let msgErro = document.querySelector(`#msgErro-${input.name}`);
+    msgErro.classList.add('display-none');
+    msgErro.innerHTML = "";
+}
--- login.php
+++ login.php
@@ -77,6 +77,9 @@
     <script src="JavaScript/exibirOuOcultarSenhaInput.js"></script>
     <script src="JavaScript/Services/HttpService.js"></script>
     <script src="JavaScript/Services/MensagemLateralService.js"></script>
+    <script src="JavaScript/cadastroCliente.js"></script>
+
+
     <script>
 
         //ALTERNAR ENTRE O FORM DE LOGIN E CADASTRO
@@ -94,126 +97,6 @@
             btnCadastro.classList.toggle('adcBordaNoBotao')
             btnLogin.classList.toggle('adcBordaNoBotao')
         }
-
-
-        const formularioCadastro = document.querySelector(".formularioDeCadastro");
-
-        formularioDeCadastro.onsubmit = (event) => {
-
-            event.preventDefault();
-
-            const validacao = validaUsuario(
-                formularioCadastro.nome,
-                formularioCadastro.email, 
-                formularioCadastro.senha,
-                formularioCadastro.confSenha
-            );
-
-            if(!validacao){
-                return;
-            }
-
-            const httpService = new HttpService();
-
-            let loader = document.querySelector("#loaderCadastrar");
-
-            loader.classList.remove("display-none");
-
-            httpService.postFormulario(formularioCadastro, 'back-end/cadastraUsuario.php')
-            .then( resposta => {
-
-                formularioCadastro.reset();
-
-                loader.classList.add("display-none");
-                
-                location.href = "index.php";
-            } )
-            .catch( resposta => {
-
-                loader.classList.add("display-none");
-
-                if(resposta.message == 'usuario_ja_cadastrado'){
-                    abrirMensagemDeErroDoInput(formularioCadastro.email, "E-mail inválido ou já cadastrado.");
-                }else{
-                    fecharMensagemDeErroDoInput(formularioCadastro.email);
-                }
-                console.log(resposta);
-            } )
-
-        }
-
-        function validaUsuario(campoNome, campoEmail, campoSenha, campoConfSenha)
-        {
-            let nome = campoNome.value;
-            let email = campoEmail.value;
-            let senha = campoSenha.value;
-            let confSenha = campoConfSenha.value;
-
-            if(nome.trim().length == 0){
-                abrirMensagemDeErroDoInput(campoNome, "Informe seu nome.");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoNome);
-
-            if( nome.length < 3 || nome.length > 80){
-                abrirMensagemDeErroDoInput(campoNome, "Tamanho do nome inválido!");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoNome);
-
-            if(email.trim().length == 0){
-                abrirMensagemDeErroDoInput(campoEmail, "Informe um e-mail.");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoEmail);
-
-            if(email.trim().length > 260){
-                abrirMensagemDeErroDoInput(campoEmail, "E-mail inválido");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoEmail);
-
-            if(senha.trim().length == 0){
-                abrirMensagemDeErroDoInput(campoSenha, "Informe a senha.");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoSenha);
-
-            if(senha.trim().length < 8){
-                abrirMensagemDeErroDoInput(campoSenha, "A senha deve conter no mínimo 8 caracteres.");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoSenha);
-
-            if(confSenha.trim().length == 0){
-                abrirMensagemDeErroDoInput(campoConfSenha, "Confirme a senha.");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoConfSenha);
-
-            if(confSenha != senha){
-                abrirMensagemDeErroDoInput(campoConfSenha, "As senhas não coincidem.");
-                return false;
-            }
-            fecharMensagemDeErroDoInput(campoConfSenha);
-
-            return true;
-        }
-
-        function abrirMensagemDeErroDoInput(input, mensagem){
-            input.focus();
-
-            let msgErro = document.querySelector(`#msgErro-${input.name}`);
-            msgErro.classList.remove('display-none');
-            msgErro.innerHTML = mensagem;
-        }
-
-        function fecharMensagemDeErroDoInput(input){
-            let msgErro = document.querySelector(`#msgErro-${input.name}`);
-            msgErro.classList.add('display-none');
-            msgErro.innerHTML = "";
-        }
-
 
     </script>
 </body>
