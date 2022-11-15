54490363ad5829d419d7497a5fbbb5a793115eef
Tiago zay <tiagozay@gmail.com>
2022-11-15 14:12:28 -0300

    Excluíndo arquivos antigos
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- JavaScript/enviarTamanhoDeTela.js
+++
@@ -1,4 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("POST", './src/acoes/salvarTamanhoDeTela.php');
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-xhr.send("tamanho="+window.innerWidth)
