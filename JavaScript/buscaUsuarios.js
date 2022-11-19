3fe88ba39c35a28f7147535b8f4d1012cea93a05
Tiago zay <tiagozay@gmail.com>
2022-11-19 17:58:35 -0300

    Implementando pesquisa por Usuário
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- JavaScript/buscaUsuarios.js
+++ JavaScript/buscaUsuarios.js
@@ -1,8 +1,9 @@
 let ordem = 'mais_antigos';
 let niveis = 'todos';
 
-let selectOrdem = document.querySelector("#ordenarPor");
-let selectNivel = document.querySelector("#niveis");
+const selectOrdem = document.querySelector("#ordenarPor");
+const selectNivel = document.querySelector("#niveis");
+const inputBuscarUsuario = document.querySelector("#buscar");
 
 selectOrdem.onchange = () => {
     ordem = selectOrdem.value;
@@ -13,6 +14,26 @@ selectNivel.onchange = () => {
     niveis = selectNivel.value;
     buscaUsuarios();
 }
+
+inputBuscarUsuario.oninput = () => {
+    let valorDigitado = inputBuscarUsuario.value;
+
+    const regExp = new RegExp(valorDigitado, 'i');
+
+    const usuariosFiltrados = listaUsuarios.filter((usuario) => regExp.test(usuario.nome))
+
+    if(usuariosFiltrados.length == 0){
+        tbdoy.innerHTML = `
+        <tr id='mensagemSemRegistros'>
+            <td colspan='4'>Nenhum resultado encontardo!</td>
+        </tr>`;
+        return;
+    }
+
+    escreveUsuariosNaLista(usuariosFiltrados);
+}
+
+let listaUsuarios = [];
 
 const quantidadeUsuarios = document.querySelector(".quantidadeUsuarios");
 
@@ -58,53 +79,61 @@ function buscaUsuarios()
     httpService.get(`back-end/buscaUsuarios.php?ordem=${ordem}&niveis=${niveis}`)
     .then( res => res.json() )
     .then( usuarios => {
+    
+        listaUsuarios = usuarios;
 
         loader.classList.add("desativarLoader");
 
-        quantidadeUsuarios.innerText = `${usuarios.length} Usuários`;
-
-        usuarios.forEach(usuario => {
-
-            let id = usuario['id'];
-            let nome = usuario['nome'];
-            let email = usuario['email'];
-            let nivel = usuario['nivel'];
-        
-            let acoesUsuario = geraCampoDeAcoes(id, nivel);
-
-
-            tbdoy.innerHTML += 
-            `
-                <tr class="tbody__tr">
-                    <td class="tbody__tr__tdGenerico tbody__tr__tdNome">
-                        <div class="tbody__tr__td__divConteudoGenerica tbody__tr__td__divNome"><p>${nome}</p></div>
-                    </td>
-
-                    <td class="tbody__tr__tdGenerico tbody__tr__tdEmail">
-                        <div class="tbody__tr__td__divConteudoGenerica tbody__tr__td__divEmail"><p>${email}</p></div>
-                    </td>
-
-                    <td class="tbody__tr__tdGenerico tbody__tr__tdNivel">
-                        <div class="tbody__tr__td__divConteudoGenerica">
-                            <p>${nivel}</p> 
-                        </div>    
-                    </td>
-                    <td  class="tbody__tr__tdAcoes">
-                        <i class="material-icons btnAbrirModal" onclick="create_modal('menu de açoes para usuários da lista')">menu</i> 
-
-                        <div class='containerAcoesItemListaUsuarios id-${id}'>
-                            ${acoesUsuario}
-                        </div>
-
-                    </td>
-                </tr>
-            `
-
-
-        });
+        escreveUsuariosNaLista(usuarios);
     } )
     .catch( msg => {
         loader.classList.add("desativarLoader");
         console.log(msg)
     } )
+}
+
+function escreveUsuariosNaLista(usuarios)
+{
+    quantidadeUsuarios.innerText = `${usuarios.length} Usuários`;
+
+    tbdoy.innerHTML = "";
+
+    usuarios.forEach(usuario => {
+
+        let id = usuario['id'];
+        let nome = usuario['nome'];
+        let email = usuario['email'];
+        let nivel = usuario['nivel'];
+    
+        let acoesUsuario = geraCampoDeAcoes(id, nivel);
+
+
+        tbdoy.innerHTML += 
+        `
+            <tr class="tbody__tr">
+                <td class="tbody__tr__tdGenerico tbody__tr__tdNome">
+                    <div class="tbody__tr__td__divConteudoGenerica tbody__tr__td__divNome"><p>${nome}</p></div>
+                </td>
+
+                <td class="tbody__tr__tdGenerico tbody__tr__tdEmail">
+                    <div class="tbody__tr__td__divConteudoGenerica tbody__tr__td__divEmail"><p>${email}</p></div>
+                </td>
+
+                <td class="tbody__tr__tdGenerico tbody__tr__tdNivel">
+                    <div class="tbody__tr__td__divConteudoGenerica">
+                        <p>${nivel}</p> 
+                    </div>    
+                </td>
+                <td  class="tbody__tr__tdAcoes">
+                    <i class="material-icons btnAbrirModal" onclick="create_modal('menu de açoes para usuários da lista')">menu</i> 
+
+                    <div class='containerAcoesItemListaUsuarios id-${id}'>
+                        ${acoesUsuario}
+                    </div>
+
+                </td>
+            </tr>
+        `
+
+    });
 }
